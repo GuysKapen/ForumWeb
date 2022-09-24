66495c14149185f69bb911e557c7349b1f13cb81
GuysKapen <guyskapen@gmail.com>
2022-09-24 16:00:44 +0700

    Setup chatbox

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
+// import Chatbox from "@/components/Chatbox.vue";
 import 'mosha-vue-toastify/dist/style.css'
+import Chatbox from './components/chatbox/Chatbox.vue'
 </script>
 
 <template>
   <div>
     <router-view />
+    <chatbox />
+    <!-- <Chatbox /> -->
   </div>
 </template>
