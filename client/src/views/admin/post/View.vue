a6e9f6493b59db0afa26e0758ce4d5916ec29e8b
GuysKapen <guyskapen@gmail.com>
2022-10-22 14:37:42 +0700

    Add approve post

--- client/src/views/admin/post/View.vue
+++ client/src/views/admin/post/View.vue
@@ -48,7 +48,7 @@ import moment from 'moment';
                                                         class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                                         Status
                                                     </th>
-                                                     <th scope="col"
+                                                    <th scope="col"
                                                         class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider whitespace-nowrap">
                                                         Answers
                                                     </th>
@@ -57,7 +57,7 @@ import moment from 'moment';
                                                         Created At
                                                     </th>
                                                     <th scope="col"
-                                                        class="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                        class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
                                                         Action
                                                     </th>
                                                 </tr>
@@ -70,13 +70,18 @@ import moment from 'moment';
                                                     <td class="text-sm font-medium text-gray-900 px-6">{{ post.title
                                                     }}</td>
                                                     <td class="px-6 py-4 whitespace-nowrap">
-                                                        <span
+                                                        <span v-if="post.status"
                                                             class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
                                                             Active
                                                         </span>
+                                                        <span v-else
+                                                            class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
+                                                            Pending
+                                                        </span>
                                                     </td>
                                                     <td class="text-sm text-center font-medium text-indigo-800 px-6">
-                                                        <router-link :to="{name: 'answer-of-post', params: {'postId': post._id}}"
+                                                        <router-link
+                                                            :to="{name: 'answer-of-post', params: {'postId': post._id}}"
                                                             class="text-indigo-600 hover:text-indigo-800">
                                                             {{post.answers.length}}
                                                         </router-link>
@@ -86,13 +91,25 @@ import moment from 'moment';
                                                         1000 )).format('DD/MM/YYYYY')}}
                                                     </td>
                                                     <td
-                                                        class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                                        <a href="{{ route('admin.language.edit', company._id) }}"
-                                                            class="text-indigo-600 hover:text-indigo-900 mx-2">Edit</a>
+                                                        class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium flex justify-center">
+                                                        <button
+                                                            class="p-3 mx-1 rounded-lg hover:bg-indigo-600 hover:text-white text-gray-400"
+                                                            type="button" @click="approveModel(post._id)">
+                                                            <svg viewBox="0 0 512 512" style="width: 1em;">
+                                                                <path fill="currentColor"
+                                                                    d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 464c-118.664 0-216-96.055-216-216 0-118.663 96.055-216 216-216 118.664 0 216 96.055 216 216 0 118.663-96.055 216-216 216zm141.63-274.961L217.15 376.071c-4.705 4.667-12.303 4.637-16.97-.068l-85.878-86.572c-4.667-4.705-4.637-12.303.068-16.97l8.52-8.451c4.705-4.667 12.303-4.637 16.97.068l68.976 69.533 163.441-162.13c4.705-4.667 12.303-4.637 16.97.068l8.451 8.52c4.668 4.705 4.637 12.303-.068 16.97z">
+                                                                </path>
+                                                            </svg>
+                                                        </button>
 
-                                                        <button class="text-red-600 hover:text-red-900" type="button"
-                                                            @click="deleteModel(post._id)">
-                                                            Delete
+                                                        <button
+                                                            class="p-3 mx-1 rounded-lg hover:bg-indigo-600 hover:text-white text-gray-400"
+                                                            type="button" @click="deleteModel(post._id)">
+                                                            <svg viewBox="0 0 448 512" style="width: 1em;">
+                                                                <path fill="currentColor"
+                                                                    d="M296 432h16a8 8 0 0 0 8-8V152a8 8 0 0 0-8-8h-16a8 8 0 0 0-8 8v272a8 8 0 0 0 8 8zm-160 0h16a8 8 0 0 0 8-8V152a8 8 0 0 0-8-8h-16a8 8 0 0 0-8 8v272a8 8 0 0 0 8 8zM440 64H336l-33.6-44.8A48 48 0 0 0 264 0h-80a48 48 0 0 0-38.4 19.2L112 64H8a8 8 0 0 0-8 8v16a8 8 0 0 0 8 8h24v368a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V96h24a8 8 0 0 0 8-8V72a8 8 0 0 0-8-8zM171.2 38.4A16.1 16.1 0 0 1 184 32h80a16.1 16.1 0 0 1 12.8 6.4L296 64H152zM384 464a16 16 0 0 1-16 16H80a16 16 0 0 1-16-16V96h320zm-168-32h16a8 8 0 0 0 8-8V152a8 8 0 0 0-8-8h-16a8 8 0 0 0-8 8v272a8 8 0 0 0 8 8z">
+                                                                </path>
+                                                            </svg>
                                                         </button>
                                                     </td>
                                                 </tr>
@@ -146,6 +163,22 @@ export default {
                     createToast('Success delete post', { type: 'success' })
                 })
             })
+        },
+        approveModel(id) {
+            const self = this;
+            showConfirmPopup(function () {
+                const authStore = useAuthStore()
+                axios.put(`${serverUrl}/posts/${id}/approve`, { status: true }, {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${authStore.token}`,
+                        "x-access-token": authStore.token,
+                    },
+                }).then(function () {
+                    self.posts = self.posts.map(item => item._id === id ? Object.assign(item, { status: true }) : item);
+                    createToast('Success approve post', { type: 'success' })
+                })
+            }, "Do you really want to approve?", "Approve")
         }
     }
 }
--- server/src/controllers/posts.js
+++ server/src/controllers/posts.js
@@ -115,3 +115,12 @@ exports.answer = function (req, res) {
     });
   })
 };
+
+exports.approve = function (req, res) {
+  const attrs = { status: true }
+  Post.findOneAndUpdate({ _id: req.params.id }, attrs, function (err, item) {
+    if (err) return response.sendBadRequest(res, err);
+    if (!req.currentUser.canEdit(item)) return response.sendForbidden(res);
+    res.json(item);
+  });
+};
