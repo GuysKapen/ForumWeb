3d4273240b0f8b9919011f14898ef4d9324fd190
GuysKapen <guyskapen@gmail.com>
2022-12-14 16:20:48 +0700

    Fix admin company form

--- client/src/components/AdminCompanyForm.vue
+++ client/src/components/AdminCompanyForm.vue
@@ -5,30 +5,105 @@
         <div>
             <form enctype="multipart/form-data" @submit.prevent="submit()">
 
-                <div class="mb-4 px-2 w-full">
-                    <div class="field">
-                        <div class="control">
-                            <label class="block font-semibold text-sm" for="name">Company name</label>
-                            <input id="name" name="name" v-model="newModel.name"
-                                class="string w-full px-4 py-3 rounded-lg font-medium bg-gray-100 border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:shadow-md focus:border-gray-400 focus:bg-white my-2"
-                                type="text" autofocus placeholder="Name..." />
+                <div class="grid grid-cols-6 gap-6">
+                    <div class="col-span-6 w-full">
+                        <div class="field">
+                            <div class="control">
+                                <label class="block font-semibold text-sm" for="name">Company name</label>
+                                <input id="name" name="name" v-model="newModel.name"
+                                    class="string w-full rounded-lg font-medium bg-gray-100 border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:shadow-md focus:border-gray-400 focus:bg-white my-2"
+                                    type="text" autofocus placeholder="Name..." />
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="mb-4 px-2 w-full">
-                    <div class="field">
-                        <div class="control">
-                            <label class="block font-semibold text-sm" for="name">Company address</label>
-                            <input id="name" name="name" v-model="newModel.address"
-                                class="string w-full px-4 py-3 rounded-lg font-medium bg-gray-100 border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:shadow-md focus:border-gray-400 focus:bg-white my-2"
-                                type="text" autofocus placeholder="Address..." />
+                    <div class="col-span-6 w-full">
+                        <div class="field">
+                            <div class="control">
+                                <label class="block font-semibold text-sm" for="name">Company address</label>
+                                <input id="name" name="name" v-model="newModel.address"
+                                    class="string w-full rounded-lg font-medium bg-gray-100 border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:shadow-md focus:border-gray-400 focus:bg-white my-2"
+                                    type="text" autofocus placeholder="Address..." />
+                            </div>
                         </div>
                     </div>
-                </div>
 
+                    <div class="col-span-6">
+                        <label for="country" class="block text-sm font-medium text-gray-700">Country</label>
+                        <select id="country" name="country" autocomplete="country-name" v-model="newModel.country"
+                            class="
+                            mt-1
+                            block
+                            w-full
+                            py-2
+                            px-3
+                            border border-gray-300
+                            rounded-md
+                            shadow-sm
+                            focus:outline-none
+                            bg-gray-100 focus:bg-white
+                            focus:ring-indigo-500
+                            focus:border-indigo-500
+                            sm:text-sm
+                          ">
+                            <option>United States</option>
+                            <option>Canada</option>
+                            <option>Mexico</option>
+                        </select>
+                    </div>
+
+                    <div class="col-span-6 sm:col-span-6 lg:col-span-2">
+                        <label for="city" class="block text-sm font-medium text-gray-700">City</label>
+                        <input type="text" name="city" id="city" v-model="newModel.city" autocomplete="address-level2"
+                            class="
+                            mt-1
+                            focus:ring-indigo-500 focus:border-indigo-500
+                            block
+                            w-full
+                            shadow-sm
+                            sm:text-sm
+                            bg-gray-100 focus:bg-white
+                            border-gray-300
+                            rounded-md
+                          " />
+                    </div>
+
+                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label for="region" class="block text-sm font-medium text-gray-700">State / Province</label>
+                        <input type="text" name="region" id="region" v-model="newModel.state"
+                            autocomplete="address-level1" class="
+                            mt-1
+                            focus:ring-indigo-500 focus:border-indigo-500
+                            block
+                            w-full
+                            shadow-sm
+                            sm:text-sm
+                            bg-gray-100 focus:bg-white
+                            border-gray-300
+                            rounded-md
+                          " />
+                    </div>
+
+                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label for="postal-code" class="block text-sm font-medium text-gray-700">ZIP / Postal
+                            code</label>
+                        <input type="text" name="postal-code" id="postal-code" v-model="newModel.zipcode"
+                            autocomplete="postal-code" class="
+                            mt-1
+                            focus:ring-indigo-500 focus:border-indigo-500
+                            block
+                            w-full
+                            shadow-sm
+                            sm:text-sm
+                            bg-gray-100 focus:bg-white
+                            border-gray-300
+                            rounded-md
+                          " />
+                    </div>
+
+                </div>
                 <div class="py-3 text-right px-2 flex justify-between">
-                    <router-link :to="{name: 'company-index'}"
+                    <router-link :to="{ name: 'company-index' }"
                         class="inline-flex justify-center py-2 px-8 rounded-full border border-transparent shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                         Back
                     </router-link>
